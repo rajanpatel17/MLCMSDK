f850fb1ccd7544122e23bc4cf386e4304689a182
Rajan Patel <rajan.patel@mintoak.com>
2026-07-01 12:21:24 +0530

    - Fix Floating Video Fullscreen & Aspect Ratio

--- Sources/MLCMSDK/PopupControllers/FlotingViewController.swift
+++ Sources/MLCMSDK/PopupControllers/FlotingViewController.swift
@@ -4,12 +4,13 @@
 import Foundation
 import UIKit
 import AVKit
+import AVFoundation
 
-class FlotingViewController{
+class FlotingViewController: NSObject {
     
     var superVC: UIViewController?
     var player: AVPlayer!
-    var playerLayer: AVPlayerLayer!
+    var playerViewController = AVPlayerViewController()
     var playerRateObservation: NSKeyValueObservation?
     var floatingView: UIView!
     
@@ -17,7 +18,15 @@ class FlotingViewController{
         superVC = vc
         let videoURL = URL(string: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!
         player = AVPlayer(url: videoURL)
-        playerLayer = AVPlayerLayer(player: player)
+        
+        playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        
+        // Prevents the video from taking over the screen when play is tapped
+        playerViewController.entersFullScreenWhenPlaybackBegins = false
+        // Adjusts the scaling (e.g., to maintain aspect ratio without stretching)
+        playerViewController.videoGravity = .resizeAspect
+        playerViewController.showsPlaybackControls = false
         
         // Create a floating view to hold the player
         floatingView = UIView(frame: CGRect(x: 20, y: (superVC?.view.frame.maxY ?? 0) - 300, width: 150, height: 250))
@@ -37,6 +46,9 @@ class FlotingViewController{
     
     @objc func closeFloatingView() {
         floatingView.removeFromSuperview()
+        playerViewController.view.removeFromSuperview()
+        playerViewController.willMove(toParent: nil)
+        playerViewController.removeFromParent()
     }
     
     @objc func maximizeFloatingView() {
@@ -46,13 +58,21 @@ class FlotingViewController{
     func setFloatingView(){
         floatingView.removeFromSuperview()
         
-        playerLayer.frame = floatingView.bounds
-        playerLayer.frame.size.height = floatingView.frame.size.height
-        playerLayer.frame.size.width = floatingView.frame.size.width
-        playerLayer.videoGravity = .resizeAspectFill
-        playerLayer.cornerRadius = 10
-        floatingView.layer.addSublayer(playerLayer)
+        playerViewController.view.removeFromSuperview()
+        playerViewController.willMove(toParent: nil)
+        playerViewController.removeFromParent()
         
+        playerViewController.view.frame = floatingView.bounds
+        playerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        playerViewController.showsPlaybackControls = false
+        
+        if let superVC = superVC {
+            superVC.addChild(playerViewController)
+            floatingView.addSubview(playerViewController.view)
+            playerViewController.didMove(toParent: superVC)
+        } else {
+            floatingView.addSubview(playerViewController.view)
+        }
         
         let closeButton = UIButton(frame: CGRect(x: 5, y: 10, width: 50, height: 30))
         closeButton.setTitle("Close", for: .normal)
@@ -71,36 +91,70 @@ class FlotingViewController{
     }
     
     func transitionToFullScreen() {
-//        playerLayer.removeFromSuperlayer()
+        playerViewController.willMove(toParent: nil)
+        playerViewController.view.removeFromSuperview()
+        playerViewController.removeFromParent()
         
-        let playerViewController = AVPlayerViewController()
-        playerViewController.player = player
-        playerViewController.player?.play()
-        playerViewController.videoGravity = .resizeAspectFill
+        // Prevents the video from taking over the screen when play is tapped
+        playerViewController.entersFullScreenWhenPlaybackBegins = false
+        // Adjusts the scaling (e.g., to maintain aspect ratio without stretching)
+        playerViewController.videoGravity = .resizeAspect
+        
         playerViewController.showsPlaybackControls = false
         playerViewController.allowsPictureInPicturePlayback = false
         playerViewController.exitsFullScreenWhenPlaybackEnds = false
         playerViewController.isModalInPresentation = true
-        superVC?.present(playerViewController, animated: true) {
-            let closeButton = UIButton(frame: CGRect(x: 20, y: 20, width: 50, height: 50))
-            closeButton.setTitle("Close", for: .normal)
-            closeButton.addTarget(self, action: #selector(self.dismissFullScreen), for: .touchUpInside)
-            playerViewController.view.addSubview(closeButton)
+        playerViewController.modalPresentationStyle = .fullScreen
+        
+        superVC?.present(playerViewController, animated: true) { [weak self] in
+            guard let self = self else { return }
             
-            let minimizeButton = UIButton(frame: CGRect(x: playerViewController.view.bounds.width - 70, y: 20, width: 50, height: 50))
-            minimizeButton.setTitle("Minimize", for: .normal)
-            minimizeButton.addTarget(self, action: #selector(self.minimizeToFloatingView), for: .touchUpInside)
-            playerViewController.view.addSubview(minimizeButton)
+            if let overlayView = self.playerViewController.contentOverlayView {
+                overlayView.subviews.forEach { $0.removeFromSuperview() }
+                
+                let closeButton = UIButton(type: .system)
+                closeButton.setTitle("Close", for: .normal)
+                closeButton.setTitleColor(.white, for: .normal)
+                closeButton.addTarget(self, action: #selector(self.dismissFullScreen), for: .touchUpInside)
+                closeButton.translatesAutoresizingMaskIntoConstraints = false
+                overlayView.addSubview(closeButton)
+                
+                let minimizeButton = UIButton(type: .system)
+                minimizeButton.setTitle("Minimize", for: .normal)
+                minimizeButton.setTitleColor(.white, for: .normal)
+                minimizeButton.addTarget(self, action: #selector(self.minimizeToFloatingView), for: .touchUpInside)
+                minimizeButton.translatesAutoresizingMaskIntoConstraints = false
+                overlayView.addSubview(minimizeButton)
+                
+                NSLayoutConstraint.activate([
+                    closeButton.leadingAnchor.constraint(equalTo: overlayView.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+                    closeButton.topAnchor.constraint(equalTo: overlayView.safeAreaLayoutGuide.topAnchor, constant: 20),
+                    closeButton.widthAnchor.constraint(equalToConstant: 70),
+                    closeButton.heightAnchor.constraint(equalToConstant: 44),
+                    
+                    minimizeButton.trailingAnchor.constraint(equalTo: overlayView.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+                    minimizeButton.topAnchor.constraint(equalTo: overlayView.safeAreaLayoutGuide.topAnchor, constant: 20),
+                    minimizeButton.widthAnchor.constraint(equalToConstant: 90),
+                    minimizeButton.heightAnchor.constraint(equalToConstant: 44)
+                ])
+            }
         }
     }
     
     @objc func dismissFullScreen() {
         floatingView.removeFromSuperview()
-        superVC?.dismiss(animated: true, completion: nil)
+        superVC?.dismiss(animated: true) { [weak self] in
+            guard let self = self else { return }
+            self.playerViewController.willMove(toParent: nil)
+            self.playerViewController.view.removeFromSuperview()
+            self.playerViewController.removeFromParent()
+        }
     }
     
     @objc func minimizeToFloatingView() {
-        superVC?.dismiss(animated: true)
+        superVC?.dismiss(animated: true) { [weak self] in
+            self?.setFloatingView()
+        }
     }
     
 }
